32996eae9ba70327281e4a92a4aae257fb5101f6
Ben Dunkle <ben@field2.com>
2025-05-22 11:15:17 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Lazy Fade
+
+Contributors: EmpireOfLight
+Tags: UI, lazyload
+Requires at least: 7.0
+Tested up to: 6.8.1
+Stable tag: 1.0
+License: GPL-2.0
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+## Description
+
+Lazy Fade is a simple plugin to enhance the presentation of your site. As the user scrolls, content will slightly fade and animate into view, adding a more natural look and feel to the experience. Furthermore, images "below the fold" will only load when they come into view, saving download time and optimizing your site.
+
+## Features
+
+- Content will smoothly glide and fade into view.
+- Images will load only when they appear when scrolling.
+- Free!
+
+## Installation
+
+1. Upload the `lazy-fade` folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. That's it!
+
+## License
+
+This project is licensed under the GNU GPL-2.0.
---
+++ tags/1.0/lazy-fade.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Plugin Name: Lazy Fade
+ * Description: Lazy-load images and fade-in wp-block-group elements on scroll.
+ * Version: 1.0
+ * Author: Ben Dunkle
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Enqueue scripts and styles
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_script(
+        'lazy-fade-script',
+        plugin_dir_url(__FILE__) . 'lazy-fade.js',
+        [],
+        '1.1',
+        true
+    );
+    wp_enqueue_style(
+        'lazy-fade-style',
+        plugin_dir_url(__FILE__) . 'lazy-fade.css'
+    );
+});
+
+// Add lazy class to images in the_content
+add_filter('the_content', function ($content) {
+    return preg_replace_callback(
+        '/<img(.*?)src=["\'](.*?)["\'](.*?)>/i',
+        function ($matches) {
+            $attrs = $matches[1] . $matches[3];
+            $src = $matches[2];
+            return '<img class="lazy-fade" data-src="' . esc_attr($src) . '" ' . $attrs . '>';
+        },
+        $content
+    );
+});
+
+// Add lazy class to post thumbnails (featured images)
+add_filter('post_thumbnail_html', function ($html) {
+    return preg_replace_callback(
+        '/<img(.*?)src=["\'](.*?)["\'](.*?)>/i',
+        function ($matches) {
+            $attrs = $matches[1] . $matches[3];
+            $src = $matches[2];
+            return '<img class="lazy-fade" data-src="' . esc_attr($src) . '" ' . $attrs . '>';
+        },
+        $html
+    );
+});
+
+// Add fade-in class to wp-block-group divs
+add_filter('render_block', function ($block_content, $block) {
+    if ($block['blockName'] === 'core/group') {
+        $block_content = preg_replace(
+            '/<div class="wp-block-group/',
+            '<div class="wp-block-group group-fade-in',
+            $block_content,
+            1
+        );
+    }
+    return $block_content;
+}, 10, 2);
